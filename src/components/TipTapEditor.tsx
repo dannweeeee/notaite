c958c7df7bbb1abbc660fb31996d7d8467eb686b
Dann Wee <dannweezj@gmail.com>
2023-10-10 01:57:03 +0700

    Debounce Save

--- src/components/TipTapEditor.tsx
+++ src/components/TipTapEditor.tsx
@@ -5,24 +5,66 @@ import { EditorContent, useEditor } from '@tiptap/react'
 import { StarterKit } from '@tiptap/starter-kit'
 import TipTapMenuBar from './TipTapMenuBar'
 import { Button } from './ui/button'
+import { useDebounce } from '@/lib/useDebounce'
+import { useMutation } from '@tanstack/react-query'
+import axios from 'axios'
+import { NoteType } from '@/lib/db/schema'
+import Text from '@tiptap/extension-text'
 
-type Props = {}
+type Props = {note: NoteType}
 
-const TipTapEditor = (props: Props) => {
-    const [ editorState, setEditorState ] = React.useState('');
+const TipTapEditor = ({note}: Props) => {
+    const [ editorState, setEditorState ] = React.useState(note.editorState || `<h1>${note.name}</h1>`);
+    const saveNote = useMutation({
+        mutationFn: async () => {
+          const response = await axios.post("/api/saveNote", {
+            noteId: note.id,
+            editorState,
+          });
+          return response.data;
+        },
+    });
+
+    const customText = Text.extend({
+        addKeyboardShortcuts() {
+            return {
+                'Shift-a': () => {
+                    console.log("Activate AI");
+                    return true;
+                },
+            };
+        },
+    });
+
     const editor = useEditor ({
         autofocus: true, // automatically focus on the editor when the page loads
         extensions: [StarterKit], // add the starter kit extension
         content: editorState,
-        onUpdate:({editor}) => {
+        onUpdate:({ editor }) => {
             setEditorState(editor.getHTML()); // get the HTML back and bind it back to the internal local state
         },
-    })
+    });
+    const debouncedEditorState = useDebounce(editorState, 500);
+    React.useEffect(() => {
+        // save to db
+        if (debouncedEditorState === "") return;
+        saveNote.mutate(undefined, {
+            onSuccess: (data) => {
+                console.log("success update!", data);
+            },
+            onError: (err) => {
+                console.error(err);
+            },
+        });
+    }, [debouncedEditorState]);
+
   return (
     <>
         <div className="flex">
             {editor && <TipTapMenuBar editor={editor}/> }
-            <Button>Saved</Button>
+            <Button disabled variant={"outline"}>
+                {saveNote.isLoading ? "Saving..." : "Saved"}
+            </Button>
         </div>
         <div className="prose">
             <EditorContent editor={editor} />

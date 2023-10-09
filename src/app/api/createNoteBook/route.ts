14dc7fa4926af9ab7fa7c62f707d02f859afa24d
Dann Wee <dannweezj@gmail.com>
2023-10-09 16:33:46 +0700

    openai generate image prompt

--- src/app/api/createNoteBook/route.ts
+++ src/app/api/createNoteBook/route.ts
@@ -1,7 +1,8 @@
-// map to /api/createNoteBook
-
+import { generateImagePrompt } from '@/lib/openai';
 import { auth } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
+
+// map to /api/createNoteBook
 
 export async function POST(req: Request) {
     const { userId } = auth(); // get the user id from the request
@@ -12,5 +13,8 @@ export async function POST(req: Request) {
 
     const body = await req.json(); // convert the request body into json and get the body of the request
     const { name } = body
-    
+
+    const image_description = await generateImagePrompt(name);
+    console.log({image_description});
+    return new NextResponse('ok');
 }

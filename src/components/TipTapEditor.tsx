a4bbd5a0aaf8e0d2deec6775d2e1f54123082b7b
Dann Wee <dannweezj@gmail.com>
2023-10-10 02:45:42 +0700

    AI autocomplete

--- src/components/TipTapEditor.tsx
+++ src/components/TipTapEditor.tsx
@@ -10,11 +10,17 @@ import { useMutation } from '@tanstack/react-query'
 import axios from 'axios'
 import { NoteType } from '@/lib/db/schema'
 import Text from '@tiptap/extension-text'
+import { useCompletion } from 'ai/react'
 
 type Props = {note: NoteType}
 
 const TipTapEditor = ({note}: Props) => {
     const [ editorState, setEditorState ] = React.useState(note.editorState || `<h1>${note.name}</h1>`);
+
+    const { complete, completion } = useCompletion({
+        api: "/api/completion",
+    }); // complete is a function we call to trigger the endpoint, completion is a react state to get back all the tokens
+
     const saveNote = useMutation({
         mutationFn: async () => {
           const response = await axios.post("/api/saveNote", {
@@ -27,23 +33,35 @@ const TipTapEditor = ({note}: Props) => {
 
     const customText = Text.extend({
         addKeyboardShortcuts() {
-            return {
-                'Shift-a': () => {
-                    console.log("Activate AI");
-                    return true;
-                },
-            };
+          return {
+            "Shift-a": () => {
+              // take the last 30 words
+              const prompt = this.editor.getText().split(" ").slice(-30).join(" ");
+              complete(prompt);
+              return true;
+            },
+          };
         },
     });
 
     const editor = useEditor ({
         autofocus: true, // automatically focus on the editor when the page loads
-        extensions: [StarterKit], // add the starter kit extension
+        extensions: [StarterKit, customText], // add the starter kit extension
         content: editorState,
         onUpdate:({ editor }) => {
             setEditorState(editor.getHTML()); // get the HTML back and bind it back to the internal local state
         },
     });
+
+    const lastCompletion = React.useRef("");
+
+    React.useEffect(() => {
+        if (!completion || !editor) return;
+        const diff = completion.slice(lastCompletion.current.length);
+        lastCompletion.current = completion;
+        editor.commands.insertContent(diff);
+    }, [completion, editor]);
+
     const debouncedEditorState = useDebounce(editorState, 500);
     React.useEffect(() => {
         // save to db
@@ -66,9 +84,17 @@ const TipTapEditor = ({note}: Props) => {
                 {saveNote.isLoading ? "Saving..." : "Saved"}
             </Button>
         </div>
-        <div className="prose">
+        <div className="prose prose-sm w-full mt-4">
             <EditorContent editor={editor} />
         </div>
+        <div className="h-4"></div>
+        <span className="text-sm font-semibold">
+            Tip: Press{" "}
+            <kbd className="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg">
+                Shift + A
+            </kbd>
+            {" "}for AI autocomplete
+        </span>
     </>
   )
 }
